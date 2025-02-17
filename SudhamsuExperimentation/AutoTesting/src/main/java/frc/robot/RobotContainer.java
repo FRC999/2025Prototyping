d7512940f391f2b823565fda1f829eff931a0011
Sudhamsu <academyoffice79@gmail.com>
2025-02-17 12:05:55 -0500

    Adding drive controller

--- SudhamsuExperimentation/AutoTesting/src/main/java/frc/robot/RobotContainer.java
+++ SudhamsuExperimentation/AutoTesting/src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,13 @@
 package frc.robot;
 
 import frc.robot.Constants.OIConstants;
+import frc.robot.Constants.OIConstants.ControllerDevice;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import java.lang.ModuleLayer.Controller;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,6 +25,11 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+
+  public static Controller xboxController;
+  public static boolean isAllianceRed = false;
+  public static boolean isReversingControllerAndIMUForRed = true;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -49,6 +58,10 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  private void configureDriverInterface() {
+    xboxController = new Controller(ControllerDevice.XBOX_CONTROLLER);
   }
 
   /**
